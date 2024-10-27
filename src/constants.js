98cad1450c0ad738380a8c7560f88d164f2078a1
dilipjoshis <dilip@higgsbosonhealth.com>
2024-10-28 01:07:19 +0530

    changed env

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const WEB_APP_URL = "http://localhost:3000"
-export const SERVER_URL = "http://localhost:8000"
+export const WEB_APP_URL = process.env.REACT_APP_WEB_URL || "https://localhost:3000"
+export const SERVER_URL = "http://192.0.0.2:8000"
